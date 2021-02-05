ecb47b48c8f4dc03acb744cf48e39ce164a8d7e9
LAPTOP-N449OIKN\weerawut <weerawut.boon@bumail.net>
2021-02-05 20:33:09 +0700

    Send Assignment Week2

---
+++ Gi455_week2/Assets/Progarmchat/Scripts/Loginmenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using WebSocketSharp;
+
+public class Loginmenu : MonoBehaviour
+{
+    private WebSocket TwebSocket;
+    public InputField Port;
+    public InputField IP;
+    
+    public void Cheak() {
+        string PortC = "15500";
+        string IpC = "127.0.0.1";
+        
+        if (IP.text.Contains(IpC) && Port.text.Contains(PortC)) {
+            SceneManager.LoadScene("testS");
+        }
+        
+        else {
+            Debug.Log("Fail to join chat");
+        }
+    }
+}
